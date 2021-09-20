45e3469125dc62bdc6440286a213b3e118f2200e
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-09-20 17:32:38 -0500

    Desplegar nuestra lista de TODOS

--- src/app/todos/todo-list/todo-list.component.ts
+++ src/app/todos/todo-list/todo-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { Todo } from '../models/todo.model';
+import { AppState } from '../../app.reducer';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent implements OnInit {
+  todos: Todo[] = [];
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.store.select('todos').subscribe((todos) => (this.todos = todos));
   }
-
 }
--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -2,7 +2,12 @@ import { Action, createReducer, on } from '@ngrx/store';
 import { create } from './todo.actions';
 import { Todo } from './models/todo.model';
 
-export const initialState: Todo[] = [new Todo('Save the world from villains')];
+export const initialState: Todo[] = [
+  new Todo('Save the world from villains 1'),
+  new Todo('Save the world from villains 2'),
+  new Todo('Save the world from villains 3'),
+  new Todo('Save the world from villains 4'),
+];
 
 const _todoReducer = createReducer(
   initialState,
