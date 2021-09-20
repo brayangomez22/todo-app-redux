b1f2f55ccdb0acbade6a417c9168d2fb37fd7ad0
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-09-20 15:47:37 -0500

    Creating the components for our task application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { TodoModule } from './todos/todo.module';
+
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FooterComponent],
+  imports: [BrowserModule, TodoModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
