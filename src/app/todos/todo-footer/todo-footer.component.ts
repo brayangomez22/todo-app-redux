90f96a09ee588c5ccf8624261e46807581ba819d
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-09-20 20:44:35 -0500

    Change filter - string only

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import * as actions from '../../filter/filter.actions';
 
 @Component({
   selector: 'app-todo-footer',
   templateUrl: './todo-footer.component.html',
-  styleUrls: ['./todo-footer.component.css']
+  styleUrls: ['./todo-footer.component.css'],
 })
 export class TodoFooterComponent implements OnInit {
+  currentFilter: actions.validFilters = 'all';
+  filters: actions.validFilters[] = ['all', 'completed', 'pending'];
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.store
+      .select('filter')
+      .subscribe((filter) => (this.currentFilter = filter));
   }
 
+  changeFilter(filter: actions.validFilters) {
+    this.store.dispatch(actions.setFilter({ filter }));
+  }
 }
